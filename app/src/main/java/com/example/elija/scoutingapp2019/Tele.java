c83ed0e074548c6b9c2a8a64b70e8b6f33111bcd
elija <elijahgabrieljope@gmail.com>
2019-02-02 12:23:06 -0500

    Enabled buttons for recording climbing and exiting habitat to have an effect od relevant variables

--- app/src/main/java/com/example/elija/scoutingapp2019/Tele.java
+++ app/src/main/java/com/example/elija/scoutingapp2019/Tele.java
@@ -21,6 +21,11 @@ public class Tele extends AppCompatActivity implements OnClickListener {
     Button fuel_three_tele_down;
     Button hatch_three_tele_up;
     Button hatch_three_tele_down;
+    Button can_not_climb;
+    Button climb_two;
+    Button climb_three;
+    Button climb_two_team;
+    Button climb_three_team;
     TextView fuel_one_tele_entry;
     TextView hatch_one_tele_entry;
     TextView fuel_two_tele_entry;
@@ -33,6 +38,7 @@ public class Tele extends AppCompatActivity implements OnClickListener {
     public static int hatch_two_tele=0;
     public static int fuel_three_tele=0;
     public static int hatch_three_tele=0;
+    public static int climb;
 
 
     @Override
@@ -51,6 +57,11 @@ public class Tele extends AppCompatActivity implements OnClickListener {
         fuel_three_tele_down = findViewById(R.id.fuel_three_tele_down);
         hatch_three_tele_up = findViewById(R.id.hatch_three_tele_up);
         hatch_three_tele_down = findViewById(R.id.hatch_three_tele_down);
+        can_not_climb = findViewById(R.id.can_not_climb);
+        climb_two = findViewById(R.id.climb_two);
+        climb_three = findViewById(R.id.climb_three);
+        climb_two_team = findViewById(R.id.climb_two_team);
+        climb_three_team = findViewById(R.id.climb_three_team);
         fuel_one_tele_entry = findViewById(R.id.fuel_one_tele_entry);
         hatch_one_tele_entry = findViewById(R.id.hatch_one_tele_entry);
         fuel_two_tele_entry = findViewById(R.id.hatch_two_tele_entry);
@@ -70,6 +81,11 @@ public class Tele extends AppCompatActivity implements OnClickListener {
         fuel_three_tele_down.setOnClickListener(this);
         hatch_three_tele_up.setOnClickListener(this);
         hatch_three_tele_down.setOnClickListener(this);
+        can_not_climb.setOnClickListener(this);
+        climb_two.setOnClickListener(this);
+        climb_three.setOnClickListener(this);
+        climb_two_team.setOnClickListener(this);
+        climb_three_team.setOnClickListener(this);
         fuel_one_tele_entry.setText(String.valueOf(fuel_one_tele));
         hatch_one_tele_entry.setText(String.valueOf(hatch_one_tele));
         fuel_two_tele_entry.setText(String.valueOf(fuel_two_tele));
@@ -143,6 +159,46 @@ public class Tele extends AppCompatActivity implements OnClickListener {
             if(hatch_three_tele<0){
                 hatch_three_tele=0;
             }
+        }
+        else if (v==can_not_climb){
+            climb=0;
+            can_not_climb.setSelected(true);
+            climb_two.setSelected(false);
+            climb_three.setSelected(false);
+            climb_two_team.setSelected(false);
+            climb_three_team.setSelected(false);
+        }
+        else if (v==climb_two){
+            climb=1;
+            can_not_climb.setSelected(false);
+            climb_two.setSelected(true);
+            climb_three.setSelected(false);
+            climb_two_team.setSelected(false);
+            climb_three_team.setSelected(false);
+        }
+        else if (v==climb_three){
+            climb=2;
+            can_not_climb.setSelected(false);
+            climb_two.setSelected(false);
+            climb_three.setSelected(true);
+            climb_two_team.setSelected(false);
+            climb_three_team.setSelected(false);
+        }
+        else if (v==climb_two_team){
+            climb=3;
+            can_not_climb.setSelected(false);
+            climb_two.setSelected(false);
+            climb_three.setSelected(false);
+            climb_two_team.setSelected(true);
+            climb_three_team.setSelected(false);
+        }
+        else if (v==climb_three_team){
+            climb=4;
+            can_not_climb.setSelected(false);
+            climb_two.setSelected(false);
+            climb_three.setSelected(false);
+            climb_two_team.setSelected(true);
+            climb_three_team.setSelected(false);
         }
 
         fuel_one_tele_entry.setText(String.valueOf(fuel_one_tele));
