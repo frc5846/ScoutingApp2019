32654643af29ca2af5f64f056e639b270ea552c9
elija <elijahgabrieljope@gmail.com>
2019-02-07 19:50:02 -0500

    Notes Added to Sandstorm and Main Activity

--- app/src/main/java/com/example/elija/scoutingapp2019/Tele.java
+++ app/src/main/java/com/example/elija/scoutingapp2019/Tele.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Tele extends AppCompatActivity implements OnClickListener {
-    Button fuel_one_tele_up;
+    Button fuel_one_tele_up;                                    //Create variables for buttons in the layout
     Button fuel_one_tele_down;
     Button hatch_one_tele_up;
     Button hatch_one_tele_down;
@@ -26,13 +26,13 @@ public class Tele extends AppCompatActivity implements OnClickListener {
     Button climb_three;
     Button climb_two_team;
     Button climb_three_team;
-    TextView fuel_one_tele_entry;
+    TextView fuel_one_tele_entry;                               //Create variables for TextViews in the layout
     TextView hatch_one_tele_entry;
     TextView fuel_two_tele_entry;
     TextView hatch_two_tele_entry;
     TextView fuel_three_tele_entry;
     TextView hatch_three_tele_entry;
-    public static int fuel_one_tele=0;
+    public static int fuel_one_tele=0;                          //Create variables for recording data in the layout
     public static int hatch_one_tele=0;
     public static int fuel_two_tele=0;
     public static int hatch_two_tele=0;
@@ -45,7 +45,7 @@ public class Tele extends AppCompatActivity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tele);
-        fuel_one_tele_up = findViewById(R.id.fuel_one_tele_up);
+        fuel_one_tele_up = findViewById(R.id.fuel_one_tele_up);                  //Attach buttons and TextViews to the variables created for them
         fuel_one_tele_down = findViewById(R.id.fuel_one_tele_down);
         hatch_one_tele_up = findViewById(R.id.hatch_one_tele_up);
         hatch_one_tele_down = findViewById(R.id.hatch_one_tele_down);
@@ -64,12 +64,12 @@ public class Tele extends AppCompatActivity implements OnClickListener {
         climb_three_team = findViewById(R.id.climb_three_team);
         fuel_one_tele_entry = findViewById(R.id.fuel_one_tele_entry);
         hatch_one_tele_entry = findViewById(R.id.hatch_one_tele_entry);
-        fuel_two_tele_entry = findViewById(R.id.hatch_two_tele_entry);
-        hatch_two_tele_entry = findViewById(R.id.fuel_two_tele_entry);
-        fuel_three_tele_entry = findViewById(R.id.hatch_three_tele_entry);
-        hatch_three_tele_entry = findViewById(R.id.fuel_three_tele_entry);
+        fuel_two_tele_entry = findViewById(R.id.fuel_two_tele_entry);
+        hatch_two_tele_entry = findViewById(R.id.hatch_two_tele_entry);
+        fuel_three_tele_entry = findViewById(R.id.fuel_three_tele_entry);
+        hatch_three_tele_entry = findViewById(R.id.hatch_three_tele_entry);
 
-        fuel_one_tele_up.setOnClickListener(this);
+        fuel_one_tele_up.setOnClickListener(this);                              //Detect whether any button has been pressed
         fuel_one_tele_down.setOnClickListener(this);
         hatch_one_tele_up.setOnClickListener(this);
         hatch_one_tele_down.setOnClickListener(this);
@@ -86,7 +86,7 @@ public class Tele extends AppCompatActivity implements OnClickListener {
         climb_three.setOnClickListener(this);
         climb_two_team.setOnClickListener(this);
         climb_three_team.setOnClickListener(this);
-        fuel_one_tele_entry.setText(String.valueOf(fuel_one_tele));
+        fuel_one_tele_entry.setText(String.valueOf(fuel_one_tele));             //Set TextViews to the values of their corresponding variables for the recording of data
         hatch_one_tele_entry.setText(String.valueOf(hatch_one_tele));
         fuel_two_tele_entry.setText(String.valueOf(fuel_two_tele));
         hatch_two_tele_entry.setText(String.valueOf(hatch_two_tele));
@@ -94,12 +94,12 @@ public class Tele extends AppCompatActivity implements OnClickListener {
         hatch_three_tele_entry.setText(String.valueOf(hatch_three_tele));
     }
 
-    public void Autonomous (View view) {
+    public void Autonomous (View view) {                                                  //Switch to sandstorm section on button press
         Intent Auto = new Intent(this, Sandstorm.class);
         startActivity(Auto);
     }
 
-    public void Notes (View view) {
+    public void Notes (View view) {                                                      //Switch to notes section on button press
         Intent notes = new Intent(this, Notes.class);
         startActivity(notes);
     }
